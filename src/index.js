4f97ab8aa2d978df24277cc4f1f73ab32f0dd2e8
thang-tran-tt <quocthang0620@gmail.com>
2021-07-26 23:54:39 +0700

    added actions and reducers for addtodo section

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import {render} from "@testing-library/react";
+import {createStore} from "redux";
 import App from './App';
+import rootReducer from "./reducers/";
+import {Provider} from 'react-redux'
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const store =
+    createStore(rootReducer);
 
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+//serviceWorker.unregister();
