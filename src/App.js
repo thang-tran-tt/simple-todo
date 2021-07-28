1992a17c3dc8ab25f0f9e3302554f24350e71e75
thang-tran-tt <quocthang0620@gmail.com>
2021-07-28 11:32:54 +0700

    added mindate to datepicker

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import AddTodo from './components/addTodo';
 import VisibeTodoList from './components/visibleTodoList';
 
 const App = () => (
-    <div className="flex items-center mt-16 flex-col">
+    <div className="flex mt-16 flex-col px-64 w-full">
       <AddTodo />
         <VisibeTodoList />
     </div>
